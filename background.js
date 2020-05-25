cf7c23a2a92486dccdf7391ae16dd1090aca554f
unknown <you@example.comlechosquad@gmail.com>
2020-05-25 18:36:58 +0300

    master

--- background.js
+++ background.js
@@ -1,14 +1,13 @@
 const ENTRIPOINT = "https://global-openapi.bithumb.pro/openapi/v1";
-const WALLET = "/spot/assetList?assetType=wallet";
+const CURRENCY = "/spot/ticker"
+const WALLET = "/spot/assetList";
 
-var api = "2c021c65e7947617f60d193c929b1246";
-var secret = "9356d6e1541385b75aa6ec59981d1ed4dcb5e9df6aec4c5f921dcd64e0ef55cd";
 var timestamp = Date.now();
 var msgNo = Math.round(Math.random() * 1000);
 var version = "V1.0.0";
 
 //add event listner on GET button
-var getButton = document.getElementById('get');
+var getButton = document.getElementById('getButton');
 getButton.addEventListener('click', getFetch);
 
 function getFetch() {
@@ -22,34 +21,40 @@ function getFetch() {
 
     const json = response => response.json()
 
-    fetch(ENTRIPOINT)
+    fetch(ENTRIPOINT + CURRENCY + "?symbol=ETH-USDT")
         .then(status)
         .then(json)
         .then(data => {
-            console.log(data)
+            var divGet = document.getElementsByClassName("value")
+            var responseValue = document.createElement('p')
+            responseValue.innerHTML = "<p>" + data.data[0].c + "</p>"
+            responseValue.className = "value"
+            divGet[0].replaceWith(responseValue)
         })
         .catch(error => {
             console.log('Request failed', error)
         })
-
 }
 
 //add event listner on POST button
-var postButton = document.getElementById('post')
+var postButton = document.getElementById('postButton')
 postButton.addEventListener('click', postFetch);
 
 function postFetch() {
+
+    var api = document.getElementById('user-apiKey');
+    var secret = document.getElementById('user-secret');
 
     //do presignature key string
     var presignature = "apiKey=" + api + "&assetType=wallet" + "&msgNo=" + msgNo + "&timestamp=" + timestamp + "&version=" + version;
 
     //do signature
-    var hash = CryptoJS.HmacSHA256(presignature, secret);
+    var hash = CryptoJS.HmacSHA256(presignature, secret.value);
     var signature = CryptoJS.enc.Base64.stringify(hash).toLocaleLowerCase();
 
     //do body request
     var signaturedPayload = {
-        apiKey: api,
+        apiKey: api.value,
         assetType: "wallet",
         msgNo: msgNo,
         timestamp: timestamp,
